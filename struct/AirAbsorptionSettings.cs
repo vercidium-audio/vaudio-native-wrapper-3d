901625d455bafd43e44d4bddef0e707c56a7a879
Mitch RTX <mitchell@vercidium.com>
2026-08-13 21:48:39 +1000

    Moving files to common repo

--- struct/AirAbsorptionSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-namespace vaudionativewrapper
-{
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate float AirAbsorptionFormulaDelegate(float distance);
-}
